e5574ea5b0b3b672810752ee6ab8c24b64b920a1
Coihue555 <kevlar555@gmail.com>
2022-03-21 10:42:25 -0300

    base

---
+++ disenos_app/lib/src/pages/headers_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HeadersPage extends StatelessWidget {
+  const HeadersPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello friend'),
+      ),
+    );
+  }
+}
