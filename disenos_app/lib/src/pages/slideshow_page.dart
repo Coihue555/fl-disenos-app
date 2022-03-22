31673abf97b2996329395f5e7a1207f73441cf58
Coihue555 <kevlar555@gmail.com>
2022-03-22 12:00:20 -0300

    asa

--- disenos_app/lib/src/pages/slideshow_page.dart
+++ disenos_app/lib/src/pages/slideshow_page.dart
@@ -1,14 +1,126 @@
+import 'package:disenos_app/src/models/slider_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+
 
 class SlideshowPage extends StatelessWidget {
   const SlideshowPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello Friend'),
+    return ChangeNotifierProvider(
+      create: (_) => SliderModel() ,
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: _Slides(
+                )
+              ),
+              _Dots()
+            ],
+          ),
+        )
       ),
+    );
+  }
+}
+
+
+class _Dots extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(0),
+          _Dot(1),
+          _Dot(2)
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  
+  final int index;
+
+  _Dot(this.index);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal:5),
+      width: 20,
+      height: 20,
+      decoration: const BoxDecoration(
+        color: Colors.grey,
+        shape: BoxShape.circle
+      ),
+    );
+  }
+}
+
+class _Slides extends StatefulWidget {
+  
+
+  @override
+  State<_Slides> createState() => _SlidesState();
+}
+
+class _SlidesState extends State<_Slides> {
+
+  final pageViewController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    pageViewController.addListener(() {
+
+      Provider.of<SliderModel>(context, listen: false).currentPage = pageViewController;
+    });
+  }
+
+  @override void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        controller: pageViewController,
+        children: [
+          _Slide('assets/svgs/slide-1.svg'),
+          _Slide('assets/svgs/slide-2.svg'),
+          _Slide('assets/svgs/slide-3.svg')
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String svg;
+  _Slide( this.svg );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.all(30),
+      child: SvgPicture.asset(svg),
     );
   }
 }
