600fbe52496cffc74851699e4c301f883781c6e9
Coihue555 <kevlar555@gmail.com>
2022-03-25 09:59:23 -0300

    emergency page

---
+++ disenos_app/lib/src/pages/pages.dart
@@ -0,0 +1,8 @@
+export 'package:disenos_app/src/pages/pinterest_page.dart';
+export 'package:disenos_app/src/pages/slideshow_page.dart';
+export 'package:disenos_app/src/pages/graficas_circulares_page.dart';
+export 'package:disenos_app/src/labs/circular_progress_page.dart';
+export 'package:disenos_app/src/retos/cuadrado_animado_page.dart';
+export 'package:disenos_app/src/pages/headers_page.dart';
+export 'package:disenos_app/src/pages/animaciones_page.dart';
+export 'package:disenos_app/src/pages/emergency_pages.dart';
